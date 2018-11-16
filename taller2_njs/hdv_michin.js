d9bca6a3b305e038349950d913b696636bb49414
Carolina O <Karittochoa@gmail.com>
2018-11-16 11:32:00 -0500

    Update hdv_michin.js

--- taller2_njs/hdv_michin.js
+++ taller2_njs/hdv_michin.js
@@ -1,5 +1,37 @@
 
 
-const path = require('path');
-var filename = path.basename('/Desktop/taller2_njs/index.html');
-console.log(filename);
+var http = require('http'),
+fs = require('fs'),
+path = require('path'),
+urls = [
+    {
+        ruta : '',
+        salida : './index.html'//Aqui se encuentra la hdv
+    },
+
+    {
+        ruta : 'contacto',//para contactar al aspirante
+        salida : './contacto.html'
+    }
+]
+
+http.createServer(function(req,res){
+var pathUrl = path.basename(req.url)
+
+urls.forEach(function(posicion){
+    if(posicion.ruta == pathUrl){
+        fs.readFile(posicion.salida, function(err, data){
+            res.writeHead(200, {'Content-Type' : 'text/html'})
+            res.end(data)
+        })
+    }
+})
+
+if(!res.finished){
+    fs.readFile('./error.html', function(err, data){
+        res.writeHead(404, {'Content-Type' : 'text/html'})
+        res.end(data)
+    })
+}
+
+}).listen(8080)
